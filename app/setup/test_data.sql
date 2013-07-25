334f389684f69faa81ef64046188c8c674ac33b4
zefredz <zefredz@gmail.com>
2013-07-25 11:17:21 +0200

    add some data to test

---
+++ app/setup/test_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user` (id, firstname, lastname, email) VALUES
+(1, 'Paris', 'Carlton', 'paris.carlton@example.com'),
+(2, 'Fred', 'Pierreafeu', 'fred@pierreafeu.com'),
+(3, 'Wilma', 'Pierreafeu', 'wilma@pierreafeu.com');
