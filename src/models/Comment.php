189abf86c70d2bd49697373946f04d9f5eee322d
zefredz <zefredz@gmail.com>
2013-07-25 13:16:42 +0200

    phpdoc + add helpers to models

--- src/models/Comment.php
+++ src/models/Comment.php
@@ -1,12 +1,23 @@
 <?php
 
+/**
+ * Paris model to represent a comment
+ */
 class Comment extends Model
 {
+    /**
+     * Helper to get the related proposition
+     * @retrun Proposition
+     */
     public function proposition()
     {
         return $this->belongs_to('Proposition');
     }
 
+    /**
+     * Helper to get the related author
+     * @return User
+     */
     public function author()
     {
         return $this->has_one('User');
--- src/models/User.php
+++ src/models/User.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * PAris model to represent a user
+ */
 class User extends Model 
 {
+    public function propositions()
+    {
+        return $this->has_many('Proposition');
+    }
+
+    public function comments()
+    {
+        return $this->has_many('Comment');
+    }
 }
