dda8847a5960ef505ea0d5d8778fbfeae67b1e4c
zefredz <zefredz@gmail.com>
2013-08-01 20:50:33 +0200

    add user list controller

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -2,6 +2,14 @@
 
 $userController = $app['controllers_factory'];
 
+$userController->get( '/', function( Silex\Application $app ) use ($twig) {
+
+    $users = Model::factory('User')->find_many();
+
+    return $twig->render( 'userlist.html',  array( 'title' => 'Greeting', 'users' => $users, 'baseurl' => BASE_URL ) );
+
+} );
+
 $userController->get( '/{id}', function( Silex\Application $app, $id ) use ($twig) {
 
     $user = Model::factory('User')->find_one($id);
