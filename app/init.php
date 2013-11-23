6f1ca973ffd233599bc92c4fd7e09e9e476e42c4
zefredz <zefredz@gmail.com>
2013-11-23 22:32:53 +0100

    add check for dependencies

--- app/init.php
+++ app/init.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+if (!$loader = include __DIR__.'/vendor/autoload.php') {
+    die('You must set up the project dependencies.');
+}
 
 // temporary code will be moved to installer
 
