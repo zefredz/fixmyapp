6ec9c7ca644d6038a14f93af5f5c652db92a5a2f
zefredz <zefredz@gmail.com>
2013-11-24 08:38:50 +0100

    fix wrong autoloader path

--- app/init.php
+++ app/init.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!$loader = include __DIR__.'/vendor/autoload.php') {
+if (!$loader = include __DIR__.'/../vendor/autoload.php') {
     die('You must set up the project dependencies.');
 }
 
